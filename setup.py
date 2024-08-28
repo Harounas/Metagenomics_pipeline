82f362d4587441c4f0b57176a85e79d2de6d034c
Harouna <140737983+Harounas@users.noreply.github.com>
2024-08-28 11:30:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Metagenomics",
+    name="Metagenomics_pipeline
+",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
