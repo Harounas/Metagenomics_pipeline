b3ebb780bc7f8546cd774953f466b166ed8bc50d
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-01 15:52:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pandas",
-        "plotly", "kaleido",'distinctipy',
+        "plotly", "kaleido",'distinctipy','numpy',
     ],
     entry_points={
         "console_scripts": [
