736727338093e52c4d96aa197a72e76f286d88e2
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-04 10:44:20 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -203,7 +203,7 @@ def generate_abundance_plots(merged_tsv_path, top_N):
                    return hex_colors
                 num_categories = len(grouped_sum[focus].unique())
                 # Generate distinct colors for each category
-                colors = generate_distant_colors(num_categories, min_distance=90)
+               # colors = generate_distant_colors(num_categories, min_distance=90)
                 #colors = base_colors[:len(unique_targets)]
                 colors = plt.get_cmap('tab20')
                 colordict = dict(zip(grouped_sum[focus].unique(), colors))
