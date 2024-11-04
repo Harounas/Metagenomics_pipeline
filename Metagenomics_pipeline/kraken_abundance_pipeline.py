5902816aa97c46c337ff2bd89fe96effe1d1979e
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-04 09:21:49 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -9,7 +9,7 @@ from .bowtie2 import run_bowtie2
 from .kraken2 import run_kraken2
 import distinctipy
 import numpy as np
-
+import matplotlib.pyplot as plt
 def process_sample(forward, reverse, base_name, bowtie2_index, kraken_db, output_dir, threads, run_bowtie, use_precomputed_reports):
     try:
         if not use_precomputed_reports:
