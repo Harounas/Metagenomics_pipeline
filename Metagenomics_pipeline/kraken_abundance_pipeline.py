a4a626b8feba512b63f178d373fc0def4cf6d174
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-04 09:16:21 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline/kraken_abundance_pipeline.py
@@ -201,10 +201,20 @@ def generate_abundance_plots(merged_tsv_path, top_N):
     # Convert RGB colors to hex format
                    hex_colors = [f"#{r:02X}{g:02x}{b:02x}" for r, g, b in colors]
                    return hex_colors
-                num_categories = len(grouped_sum[focus].unique())
+               # num_categories = len(grouped_sum[focus].unique())
                 # Generate distinct colors for each category
-                colors = generate_distant_colors(num_categories, min_distance=20)
-                colordict = dict(zip(grouped_sum[focus].unique(), colors))
+                #colors = generate_distant_colors(num_categories, min_distance=20)
+                #colordict = dict(zip(grouped_sum[focus].unique(), colors))
+                # Generate colors using 'tab20' colormap
+                colors = plt.get_cmap('tab20').colors
+
+# Ensure there are enough colors for unique items in grouped_sum[focus]
+                unique_targets = grouped_sum[focus].unique()
+                if len(unique_targets) > len(colors):
+                  raise ValueError("Not enough unique colors in 'tab20' colormap for the number of unique targets.")
+
+# Map each unique target to a color from the colormap
+                colordict = dict(zip(unique_targets, colors[:len(unique_targets)]))
                 plot_width = 1100 + 5 * len(grouped_sum[col].unique())
                 plot_height = 800 + 5 * len(grouped_sum[col].unique())
                 font_size = max(10, 14 - len(grouped_sum[col].unique()) // 10)
