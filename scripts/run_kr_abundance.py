6eacf6707bb8368a0ecc85a4ea83d00523761a24
Harouna <140737983+Harounas@users.noreply.github.com>
2024-11-04 12:34:09 +0100

    Update run_kr_abundance.py

--- scripts/run_kr_abundance.py
+++ scripts/run_kr_abundance.py
@@ -14,7 +14,18 @@ def create_sample_id_df(input_dir):
     """
     Create a DataFrame with sample IDs based on the input FASTQ file names.
     """
-    sample_ids = [os.path.basename(f).replace("_R1.fastq.gz", "").replace("_R1.fastq", "") for f in glob.glob(os.path.join(input_dir, "*_R1.fastq*"))]
+    sample_ids = []
+    for f in glob.glob(os.path.join(input_dir, "*_R1.fastq*")):
+        sample_id = os.path.basename(f)
+        sample_id = sample_id.replace("_R1.fastq.gz", "").replace("_R1.fastq", "").replace("_R1_001.fastq.gz", "").replace("_R1_001.fastq", "").replace("_R1_002.fastq.gz", "").replace("_R1_002.fastq", "")
+        sample_ids.append(sample_id)
+
+    for f in glob.glob(os.path.join(input_dir, "*R1.fastq*")):  # To capture R1 without the underscore
+        sample_id = os.path.basename(f)
+        sample_id = sample_id.replace("R1.fastq.gz", "").replace("R1.fastq", "").replace("_001.fastq.gz", "").replace("_001.fastq", "").replace("_002.fastq.gz", "").replace("_002.fastq", "")
+        if sample_id not in sample_ids:  # Avoid duplicates
+            sample_ids.append(sample_id)
+
     sample_id_df = pd.DataFrame(sample_ids, columns=["Sample_IDs"])
     return sample_id_df
 
@@ -42,19 +53,30 @@ def main():
         logging.error(f"Kraken database directory '{args.kraken_db}' not found.")
         sys.exit(1)
 
-
     run_bowtie = not args.no_bowtie2 and args.bowtie2_index is not None
 
     for forward in glob.glob(os.path.join(args.input_dir, "*_R1.fastq*")):
-        base_name = os.path.basename(forward).replace("_R1.fastq.gz", "").replace("_R1.fastq", "")
- 
-        reverse = os.path.join(args.input_dir, f"{base_name}_R2.fastq.gz") if forward.endswith(".gz") else os.path.join(args.input_dir, f"{base_name}_R2.fastq")
+        base_name = os.path.basename(forward)
+        base_name = base_name.replace("_R1.fastq.gz", "").replace("_R1.fastq", "").replace("_R1_001.fastq.gz", "").replace("_R1_001.fastq", "").replace("_R1_002.fastq.gz", "").replace("_R1_002.fastq", "")
         
-        if not os.path.isfile(reverse):
-            reverse = None
+        reverse_candidates = [
+            os.path.join(args.input_dir, f"{base_name}_R2.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}_R2.fastq"),
+            os.path.join(args.input_dir, f"{base_name}_R2_001.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}_R2_001.fastq"),
+            os.path.join(args.input_dir, f"{base_name}_R2_002.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}_R2_002.fastq"),
+            os.path.join(args.input_dir, f"{base_name}R2.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}R2.fastq"),
+            os.path.join(args.input_dir, f"{base_name}R2_001.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}R2_001.fastq"),
+            os.path.join(args.input_dir, f"{base_name}R2_002.fastq.gz"),
+            os.path.join(args.input_dir, f"{base_name}R2_002.fastq"),
+        ]
 
-        process_sample(forward, reverse, base_name, args.bowtie2_index, args.kraken_db, args.output_dir, args.threads, run_bowtie,args.use_precomputed_reports)
+        reverse = next((f for f in reverse_candidates if os.path.isfile(f)), None)
 
+        process_sample(forward, reverse, base_name, args.bowtie2_index, args.kraken_db, args.output_dir, args.threads, run_bowtie, args.use_precomputed_reports)
 
     # Load metadata or create sample ID DataFrame
     if args.no_metadata:
@@ -69,20 +91,6 @@ def main():
             logging.error(f"Metadata file '{args.metadata_file}' not found.")
             sys.exit(1)
         merged_tsv_path = aggregate_kraken_results(args.output_dir, metadata_file=args.metadata_file, read_count=args.read_count)
-
-    # Process each sample in the input directory
-    for forward in glob.glob(os.path.join(args.input_dir, "*_R1.fastq*")):
-        base_name = os.path.basename(forward).replace("_R1.fastq.gz", "").replace("_R1.fastq", "")
-        reverse = os.path.join(args.input_dir, f"{base_name}_R2.fastq.gz") if forward.endswith(".gz") else os.path.join(args.input_dir, f"{base_name}_R2.fastq")
-        
-        if not os.path.isfile(reverse):
-            logging.warning(f"Reverse file {reverse} not found. Skipping sample {base_name}.")
-            continue
-
-        try:
-            process_sample(forward, reverse, base_name, args.bowtie2_index, args.kraken_db, args.output_dir, args.threads, run_bowtie, args.use_precomputed_reports)
-        except Exception as e:
-            logging.error(f"Error processing sample {base_name}: {e}")
 
     # Generate abundance plots based on provided flags
     if merged_tsv_path and os.path.isfile(merged_tsv_path):
